8ed79bf2d395cd3057658e23d9e1b9187d11ffc9
RomanVetrov <romanvikernes@gmail.com>
2025-07-21 01:48:37 +0300

    fix lint

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -18,6 +18,7 @@ def to_str(value, depth):
 def render_stylish(diff, depth=1):
     lines = []
     indent = " " * ((depth - 1) * 4)
+    sign_indent = " " * (((depth - 1) * 4) - 2) if depth > 1 else ""
     for node in diff:
         key = node["key"]
         node_type = node["type"]
@@ -27,25 +28,24 @@ def render_stylish(diff, depth=1):
             lines.append(f"{indent}{key}: {children}")
         elif node_type == "added":
             lines.append(
-                f"{indent}+ {key}: {to_str(node['value'], depth + 1)}"
+                f"{sign_indent}+ {key}: {to_str(node['value'], depth + 1)}"
             )
         elif node_type == "removed":
             lines.append(
-                f"{indent}- {key}: {to_str(node['value'], depth + 1)}"
+                f"{sign_indent}- {key}: {to_str(node['value'], depth + 1)}"
             )
         elif node_type == "changed":
             lines.append(
-                f"{indent}- {key}: {to_str(node['old_value'], depth + 1)}"
+                f"{sign_indent}- {key}: {to_str(node['old_value'], depth + 1)}"
             )
             lines.append(
-                f"{indent}+ {key}: {to_str(node['new_value'], depth + 1)}"
+                f"{sign_indent}+ {key}: {to_str(node['new_value'], depth + 1)}"
             )
         elif node_type == "unchanged":
             lines.append(
                 f"{indent}{key}: {to_str(node['value'], depth + 1)}"
             )
     return "{\n" + "\n".join(lines) + f"\n{indent}}}"
-
 
 
 def format_stylish(diff):
